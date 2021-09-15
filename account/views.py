cb4a1a2e2057927c808c0803f5f9b877c9feb8e7
timur <timur.dootaliev.td@gmail.com>
2021-09-15 10:22:27 +0600

    my push

--- account/views.py
+++ account/views.py
@@ -1,4 +1,28 @@
 # обработчики запросов
-from django.shortcuts import render
-
+from rest_framework.views import APIView
 # Create your views here.
+
+
+class RegistrationView(APIView):
+    pass
+
+
+class ActivationView(APIView):
+    pass
+
+
+class LoginView(APIView):
+    pass
+
+
+class LogoutView(APIView):
+    pass
+
+
+class ForgotPasswordView(APIView):
+    pass
+
+
+class ChangePasswordView(APIView):
+    pass
+
