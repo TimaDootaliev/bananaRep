6e1291bbb9e9eafbe8b1604817b536f813a5448b
timur <timur.dootaliev.td@gmail.com>
2021-09-17 10:57:36 +0600

    added SMTP

--- main/serializers.py
+++ main/serializers.py
@@ -32,6 +32,7 @@ class CommentSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         request = self.context.get('request')
         validated_data['user'] = request.user
+        return super().create(validated_data)
 
 
 class CreatePublicationSerializer(serializers.ModelSerializer):
