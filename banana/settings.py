0ebb4e6d1cf8630e526783b4c1d14dcc1df8e2f5
timur <timur.dootaliev.td@gmail.com>
2021-09-17 12:31:12 +0600

    added paginaniton

--- banana/settings.py
+++ banana/settings.py
@@ -167,5 +167,7 @@ EMAIL_USE_TLS = config('EMAIL_USE_TLS')
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication'
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5
 }
